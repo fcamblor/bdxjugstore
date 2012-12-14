48992ba36d87f0804b574617c73c3e26a96dcdc3
Frédéric Camblor <fcamblor@gmail.com>
2012-12-14 01:59:12 +0100

    Allowing to edit a techno, thus, demonstrating the use of backbone model binding

--- app/scripts/views/TechnoListing.js
+++ app/scripts/views/TechnoListing.js
@@ -1,15 +1,17 @@
 define([
-    "backbone", "underscore", "hbs!templates/TechnoListing", "models/TechnoListing"
-], function(Backbone, _, viewTemplate, TechnoListing){
+    "backbone", "underscore", "hbs!templates/TechnoListing", "hbs!templates/EditTechno", "models/TechnoListing", "models/Techno"
+], function(Backbone, _, viewTemplate, editTechnoTpl, TechnoListing, Techno){
 
     var TechnoListingClass = Backbone.View.extend({
         events: {
+            "click .edit-btn": "technoEdited"
         },
 
         initialize: function(){
             TechnoListingClass.__super__.initialize.apply(this, arguments);
 
             this.technos = new TechnoListing();
+            this.editedTechno = null;
         },
 
         render: function(){
@@ -18,7 +20,12 @@ define([
             $.when(
                 currentView.technos.fetch()
             ).then(function(){
-                currentView.$el.html(viewTemplate({technos: currentView.technos.toJSON()}));
+                    var technos = currentView.technos.toJSON();
+                    currentView.$el.html(viewTemplate({technos: technos}));
+
+                currentView.$el.find("li").each(function(idx){
+                    $(this).data("techno", technos[idx]);
+                });
             });
 
 //            $.getJSON("/data/technos.json", function(technos){
@@ -26,6 +33,22 @@ define([
 //            });
 
             return this;
+        },
+
+        technoEdited: function(event){
+            var listItem = $(event.currentTarget).parent("li");
+            var targetTechno = listItem.data("techno");
+            console.log("Techno edited : "+targetTechno.title);
+
+            var editForm = $(editTechnoTpl(targetTechno));
+            listItem.replaceWith(editForm);
+
+            var modelBinder = new Backbone.ModelBinder();
+            this.editedTechno = new Techno(targetTechno);
+            modelBinder.bind(this.editedTechno, editForm);
+
+            // For debug purpose only !
+            window.techno = this.editedTechno;
         }
 
     });
